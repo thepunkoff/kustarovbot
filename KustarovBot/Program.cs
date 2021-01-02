506b4f54f92d8e5ac9fe6444fac7091bfc6e772a
thepunkoff <thepunkoff@gmail.com>
2021-01-03 01:33:58 +0300

    refactoring

--- KustarovBot/Program.cs
+++ KustarovBot/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using VkNet;
 using VkNet.Enums.Filters;
@@ -11,31 +13,41 @@ namespace KustarovBot
 {
     internal sealed class Program
     {
+        private static readonly Random _rng = new();
+        private static readonly VkApi _vkApi = new();
+        private static readonly UserMessageCounter _messageCounter = new();
+        private static readonly EventProcessor _eventProcessor;
+
+        static Program()
+        {
+            _eventProcessor = new(_vkApi);
+        }
+
         static async Task Main()
         {
-            Console.WriteLine("KustarovBot v 0.0.1");
-            var rng = new Random();
-            var vkApi = new VkApi();
-            await vkApi.AuthorizeAsync(new ApiAuthParams()
+            Console.WriteLine($"Initializing KustarovBot v {Assembly.GetExecutingAssembly().GetName().Version}");
+            await _vkApi.AuthorizeAsync(new ApiAuthParams()
             {
                 AccessToken = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "token.txt")),
-                Settings = Settings.Messages | Settings.Offline,
+                Settings = Settings.All | Settings.Offline,
             });
 
-            var processor = new EventProcessor(vkApi);
-            var messageCounter = new UserMessageCounter();
+            long authUserId = _vkApi.UserId ?? throw new Exception("VkApi.UserId was null. Should authorize using VkApi.Authorize");
+            var res = await _vkApi.Users.GetAsync(Array.Empty<long>(), ProfileFields.Domain);
+            var self = res.Single();
+            Console.WriteLine($"Authorized as {self.FirstName} {self.LastName} ({self.Domain})");
 
-            processor.OnNewMessage += async (message, user) =>
+            _eventProcessor.OnNewMessage += async (message, user) =>
             {
-                if (messageCounter.GetCount(user) % 10 == 0)
+                if (_messageCounter.GetCount(user) % 10 == 0)
                 {
                     try
                     {
-                        await vkApi.Messages.SendAsync(new VkNet.Model.RequestParams.MessagesSendParams()
+                        await _vkApi.Messages.SendAsync(new VkNet.Model.RequestParams.MessagesSendParams()
                         {
                             PeerId = user.Id,
                             Message = "Доброе время суток! Сегодня я не работаю, напишите мне в рабочий день. Спасибо за понимание!!",
-                            RandomId = rng.Next(),
+                            RandomId = _rng.Next(),
                         });
                     }
                     catch (CaptchaNeededException)
@@ -44,9 +56,9 @@ namespace KustarovBot
                     }
                 }
                 
-                messageCounter.Increment(user);
+                _messageCounter.Increment(user);
             };
-            await processor.ProcessEvents();
+            await _eventProcessor.ProcessEvents();
 
             Console.WriteLine("Exiting with OK.");
         }
