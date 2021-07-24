97c1c210f6093daf2ded87b6cf5431c0108e6942
thepunkoff <thepunkoff@gmail.com>
2021-07-25 01:41:46 +0300

    add sender to the exception message in the mail service + lazy init smtp client

--- KustarovBot.Shared/MailService.cs
+++ KustarovBot.Shared/MailService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
+using System.Reflection;
 using System.Threading.Tasks;
 using NLog;
 
@@ -11,31 +12,37 @@ namespace KustarovBot.Shared
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         
         private const string Mail = "mail";
+        
+        private SmtpClient _smtpClient;
 
         public async Task SendException(Exception exception)
         {
+            _smtpClient ??= _smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                EnableSsl = true,
+                Port = 587,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(
+                    await Configuration.GetValue("gmailAccount"),
+                    await Configuration.GetValue("gmailPassword")),
+            };
+            
+            var sender = Assembly.GetExecutingAssembly().FullName;
 #if DEBUG
+            Logger.Error($"[DEBUG] Exception message from {sender} reported by mail:\n{exception}.");
             return;
 #endif
             try
             {
-                var smtpClient = new SmtpClient("smtp.gmail.com")
-                {
-                    EnableSsl = true,
-                    Port = 587,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(await Configuration.GetValue("gmailAccount"), await Configuration.GetValue("gmailPassword")),
-                };
-
                 var mail = new MailMessage
                 {
                     From = new MailAddress(await Configuration.GetValue("gmailAccount")),
                     Subject = "KustarovBot exception",
-                    Body = exception.ToString(),
+                    Body = $"[{sender}]: {exception}",
                     To = { await Configuration.GetValue("gmailAccount") }
                 };
 
-                smtpClient.Send(mail);
+                _smtpClient.Send(mail);
                 
                 Logger.Trace($"[{Mail}] exception message reported.");
             }
